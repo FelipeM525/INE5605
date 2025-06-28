87013e0a37729848020fff5a856c3858482d0b13
Gustavo Haneiko <haneikogustavo@gmail.com>
2025-06-28 18:10:36 -0300

    dao

--- dao/dao.py
+++ dao/dao.py
@@ -5,31 +5,45 @@ class DAO(ABC):
     @abstractmethod
     def __init__(self, datasource=''):
         self.__datasource = datasource
-        self.__cache = {}
+        self.__cache = {} #é aqui que vai ficar a lista que estava no controlador. Nesse exemplo estamos usando um dicionario
         try:
             self.__load()
         except FileNotFoundError:
             self.__dump()
 
     def __dump(self):
-        with open(self.__datasource, 'wb') as f:
-            pickle.dump(self.__cache, f)
+        pickle.dump(self.__cache, open(self.__datasource, 'wb'))
 
     def __load(self):
-        with open(self.__datasource, 'rb') as f:
-            self.__cache = pickle.load(f)
+        self.__cache = pickle.load(open(self.__datasource,'rb'))
 
+    #esse método precisa chamar o self.__dump()
     def add(self, key, obj):
         self.__cache[key] = obj
-        self.__dump()
+        self.__dump()  #atualiza o arquivo depois de add novo amigo
+
+    #cuidado: esse update só funciona se o objeto com essa chave já existe
+    def update(self, key, obj):
+        try:
+            if(self.__cache[key] != None):
+                self.__cache[key] = obj #atualiza a entrada
+                self.__dump()  #atualiza o arquivo
+        except KeyError:
+            pass  # implementar aqui o tratamento da exceção
 
     def get(self, key):
-        return self.__cache.get(key)
+        try:
+            return self.__cache[key]
+        except KeyError:
+            pass #implementar aqui o tratamento da exceção
 
+    # esse método precisa chamar o self.__dump()
     def remove(self, key):
-        if key in self.__cache:
-            del self.__cache[key]
-            self.__dump()
+        try:
+            self.__cache.pop(key)
+            self.__dump() #atualiza o arquivo depois de remover um objeto
+        except KeyError:
+            pass #implementar aqui o tratamento da exceção
 
     def get_all(self):
-        return list(self.__cache.values())
+        return self.__cache.values()
